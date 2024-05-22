b3198f9147b7180e3877f3dcd40b023def2f5e70
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-22 22:18:15 +0100

    adding brand factory

--- backend + admin_panel/database/seeders/DatabaseSeeder.php
+++ backend + admin_panel/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Brand;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         Product::factory(100);
 
         Order::factory(30);
+
+        Brand::factory(5);
 
     }
 }
