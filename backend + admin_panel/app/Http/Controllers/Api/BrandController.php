65dcbd34be37c4b7cc3ee15b52a75bb0a7aa15db
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-25 23:45:00 +0100

    api brand contorller

--- backend + admin_panel/app/Http/Controllers/Api/BrandController.php
+++ backend + admin_panel/app/Http/Controllers/Api/BrandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
@@ -12,38 +13,11 @@ class BrandController extends Controller
      */
     public function index()
     {
-        //
+        return Brand::get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show(Brand $brand)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+        return $brand->load('products');
+    }   
 }
